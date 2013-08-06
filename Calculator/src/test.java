b34ed958c3322a478760d02810eec90b941c39ce
Gautam <gautam@Gautams-iMac.local>
2013-08-06 16:55:11 -0700

    calc commit

---
+++ Calculator/src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int result = calc.calculate("");
+		System.out.println("Result:" + result);
+
+	}
+
+}
